6f145258764b7e1abe4432b14d89cbdea340ca9b
TeamAbella060420 <jabella@rscm.com.sa>
2023-02-21 23:19:30 +0800

    Update user.ts

--- shareme_backend/schemas/user.ts
+++ shareme_backend/schemas/user.ts
@@ -3,6 +3,15 @@ export default {
     title: 'User',
     type: 'document',
     fields: [
-        {}
+        {
+            name: 'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
     ]
 }
